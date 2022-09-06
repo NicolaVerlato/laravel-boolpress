43997b3f2da81024521cf60a8dd1267bceaed6ee
NicolaVerlato <nicola.verlato00@gmail.com>
2022-09-06 16:03:50 +0200

    delete done

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -18,4 +18,11 @@
     </div>
 
     <a class="btn btn-primary mt-3" href="{{ route('admin.posts.edit', ['post' => $posts->id]) }}">Modifica</a>
+
+    <form action="{{ route('admin.posts.destroy', ['post' => $posts->id]) }}" method="post">
+        @csrf
+        @method('DELETE')
+
+        <input class="btn btn-danger mt-2" type="submit" value="Elimina">
+    </form>
 @endsection
